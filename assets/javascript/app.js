783a19d29e0e5d74b2d6d46d918acddf9e97d648
dps0610 <dps0610@hotmail.com>
2019-07-07 23:58:34 -0400

    added a jumbotron, the spans and id's to change information.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,14 @@
+//Create variables
 var right = 0;
 var wrong = 0;
+
+
+//create something to only be able to select one answer
+
+
+//Have a timer start when they select the first answer. 
+
+
+//create onclick for submit button that will determine if selected answers are correct,
+//keep track of wrong vs right answers, and update the respective fields in the html
 
